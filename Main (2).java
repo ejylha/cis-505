8182201afe7be6345922bd52aeeda4dc6cee7c50
ejylha <143954673+ejylha@users.noreply.github.com>
2024-09-01 21:58:05 -0400

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,11 @@
+// File: YourLastNameHelloWorld.java
+public class YourLastNameHelloWorld {
+    public static void main(String[] args) {
+        // Define String variables for the first and last names
+        String firstName = "YourFirstName";
+        String lastName = "YourLastName";
+
+        // Use printf to format and print the output
+        System.out.printf("Hello World from %s %s%n", firstName, lastName);
+    }
+}
